1ed7ba8dd332bd0f2d0753e3193478316cfe764d
kimhyunjumg <guswjddudwo@gmail.com>
2024-07-08 15:17:24 +0900

    data화면에 그리는중

--- src/pages/photo.js
+++ src/pages/photo.js
@@ -1,3 +1,14 @@
+import { useEffect, useState } from "react";
+
 export default function Photo() {
+  const [photo, setPhoto] = useState([]);
+
+  useEffect(() => {
+    const setAsyncPhoto = async () => {
+      const results = await setAsyncPhoto();
+      console.log(results);
+      setAsyncPhoto(results.data);
+    };
+  }, []);
   return <>photo</>;
 }
