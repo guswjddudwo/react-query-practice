4ad53146ec983ee123ecebafdb6ff8ee35452f33
kimhyunjumg <guswjddudwo@gmail.com>
2024-07-27 20:11:24 +0900

    photo page AlertDialog

--- src/apis/photo.js
+++ src/apis/photo.js
@@ -1,5 +1,6 @@
 import { apiInstance } from "../helpers/api-instance";
 
 export const getAllPhoto = () => apiInstance.get("photos");
+export const getPhoto = (id) => apiInstance.get(`photos/${id}`);
 
 // 데이터들을 화면에 그려오기
--- src/pages/photo.js
+++ src/pages/photo.js
@@ -1,24 +1,76 @@
 import { useEffect, useState } from "react";
-import { getAllPhoto } from "../apis/photo";
+import { getAllPhoto, getPhoto } from "../apis/photo";
 import { useQuery } from "@tanstack/react-query";
+import "./photo.css";
 
 export default function Photo() {
+  const [showDialog, setShowDialog] = useState({
+    id: null,
+    show: false,
+  });
   const [photo, setPhoto] = useState([]);
 
-  const { data } = useQuery({
-    queryKey: ["post"],
+  const { data: photos = [] } = useQuery({
+    queryKey: ["photo"],
     queryFn: getAllPhoto,
+    select: (data) => data.data,
   });
 
-  console.log(data);
+  // console.log(data);
+
+  const handleClickDialog = (id) => {
+    setShowDialog({
+      id,
+      show: true,
+    });
+  };
+
+  const handleClickHideDialog = () => {
+    setShowDialog({
+      id: null,
+      show: false,
+    });
+  };
   return (
     <div>
-      photo
-      <ul>
-        {photo.map((photo, index) => (
-          <li key={index}>{photo.title}</li>
+      <ul className="list">
+        {photos.map((photo) => (
+          <li
+            role="button"
+            key={photo.id}
+            className="list-item"
+            onClick={() => handleClickDialog(photo.id)}
+          >
+            <h1>{photo.title}</h1>
+            <p>{photo.body}</p>
+          </li>
         ))}
       </ul>
+      {showDialog.show && (
+        <AlertDialog id={showDialog.id} onClose={handleClickHideDialog} />
+      )}
     </div>
   );
 }
+
+const AlertDialog = ({ id, onClose: handleClose }) => {
+  const { data: photo, refetch } = useQuery({
+    queryKey: ["photo", id],
+    queryFn: () => getPhoto(id),
+    enabled: !!id,
+    select: (data) => data.data,
+    staleTime: Infinity,
+  });
+
+  console.log("photo", photo);
+  return (
+    <div className="alert-overlay">
+      <div className="alert-dialog">
+        <button onClick={handleClose}>close</button>
+        <button onClick={refetch}>refresh</button>
+        <h1>{photo?.title ?? ""}</h1>
+        <p>{photo?.body ?? ""}</p>
+      </div>
+    </div>
+  );
+};
