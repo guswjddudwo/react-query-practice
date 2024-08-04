dad1f261e9f32f65cd7222bea161a4607903d556
kimhyunjumg <guswjddudwo@gmail.com>
2024-08-04 11:11:00 +0900

    useInfiniteQuery

--- src/pages/photo.js
+++ src/pages/photo.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { getAllPhoto, getPhoto } from "../apis/photo";
 import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 import "./photo.css";
 
 export default function Photo() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
   const { data: photos, fetchNextPage } = useInfiniteQuery({
     queryKey: ["photos"],
     queryFn: getAllPhoto,
@@ -14,26 +18,24 @@ export default function Photo() {
       }
       return lastPageParam + 1;
     },
-    select: (data) => {
-      return data.pages.flatMap((photo) => photo.data);
-    },
+    select: (data) => data.pages.flatMap((photo) => photo.data),
   });
-  console.log(photos);
+
+  useEffect(() => {
+    isInView && fetchNextPage();
+  }, [isInView, fetchNextPage]);
 
   return (
     <div>
-      <ul className="list">
-        {photos.map((photo) => {
-          return (
-            <li key={photo.id} role="button" className="list-item">
-              <h1>{photo.title}</h1>
-              <p>{photo.url}</p>
-            </li>
-          );
-        })}
+      <ul className="list" style={{ minHeight: "100vh", marginBottom: "1px" }}>
+        {(photos ?? []).map((photo) => (
+          <li key={photo.id} role="button" className="list-item">
+            <h1>{photo.title}</h1>
+            <p>{photo.url}</p>
+          </li>
+        ))}
       </ul>
-      <button onClick={fetchNextPage}>next page</button>
-
+      <div ref={ref} />
       {/* {showDialog.show && (
         <AlertDialog id={showDialog.id} onClose={handleClickHideDialog} />
       )} */}
